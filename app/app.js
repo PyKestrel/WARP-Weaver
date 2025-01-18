96b012809f3fa0c3977e51b53b9898bd8728fb73
Ariel Torres <ariel.torres30@aol.com>
2025-01-17 23:32:41 -0500

    Added Basic Functionality

--- app/app.js
+++ app/app.js
@@ -1,3 +1,19 @@
+const ProtectedIDs = ["AutoConnect",
+    "ClientID",
+    "ClientSecret",
+    "DisplayName",
+    "MultiUser",
+    "OrganizationName",
+    "OverrideAPIEndpoint",
+    "OverrideDoHEndpoint",
+    "OverrideWARPEndpoint",
+    "ServiceMode",
+    "SupportURL",
+    "SwitchLock",
+    "form"
+]
+
+// Get Select Field Data From Each Form Found On The Document
 function getAllFormData() {
     // Declarations
     let FormsArray = [];
@@ -12,10 +28,12 @@ function getAllFormData() {
         let formDataValuesObject = {}
         for (let i = 0; i < x.length; i++) {
             // Check For Elements Without An ID & Skip Them
-            if(x.elements[i].id == "" || x.elements[i].id == null || x.elements[i].id == undefined){
+            if (x.elements[i].id == "" || x.elements[i].id == null || x.elements[i].id == undefined) {
+                continue;
+            }else if (!(ProtectedIDs.includes(x.elements[i].id))){
                 continue;
             }
-            else{
+            else {
                 let ID;
                 let Value;
                 // Check For Toggles/Switches & Their Corresponding States
@@ -24,7 +42,7 @@ function getAllFormData() {
                     Value = x.elements[i].checked;
                 }
                 // Elements That Arent Switches
-                else{
+                else {
                     ID = x.elements[i].id;
                     Value = x.elements[i].value;
                 }
@@ -36,4 +54,84 @@ function getAllFormData() {
         FormsArray.push(formDataValuesObject)
     }
     return FormsArray
+}
+
+let NumberOfAdditionalOrganizations = 0;
+function addOrganization() {
+    // Configuration Container
+    const ConfigGenerator = document.getElementById("ConfigGenerator");
+    // Get Configuration Buttons
+    const configurationButtonsNode = document.getElementById("ConfigurationButtons");
+    // Get All Organization Elements On DOM & Clone Only One Of Them
+    const elements = document.querySelectorAll('.organization');
+    let organizationCard = elements[0].cloneNode(true);
+    // Remove The Pre-Logon & Multi-User Options Since We Only Need It Set Once
+    //organizationCard.removeChild(document.getElementById("prelogonAccordionItem"));
+    //organizationCard.removeChild(document.getElementById("multiuserAccordionItem"));
+
+    const elementsThatHaveId = [...organizationCard.querySelectorAll('[id]')];
+    const elementsThatHaveFor = [...organizationCard.querySelectorAll('[for]')];
+    
+    elementsThatHaveId.forEach((e) => {
+        if (ProtectedIDs.includes(e.id)){
+            
+        }else{
+            if (isNaN(e.id[e.id.length - 1])){
+                let shouldSkip = false;
+                elementsThatHaveFor.forEach((f) => {
+                    if (shouldSkip) {
+                        return;
+                    }
+                    if (f.for == e.id){
+                        f.for = e.id + NumberOfAdditionalOrganizations;
+                        shouldSkip = true;
+                        return;
+                    }
+                });
+                if(e.hasAttribute('data-bs-target')){
+                    e.setAttribute('data-bs-target', e.getAttribute("data-bs-target") + NumberOfAdditionalOrganizations);
+                }
+                if(e.hasAttribute('aria-controls')){
+                    e.setAttribute('aria-controls', e.getAttribute("aria-controls") + NumberOfAdditionalOrganizations);
+                }
+                if(e.hasAttribute('data-bs-parent')){
+                    e.setAttribute('data-bs-parent', e.getAttribute("data-bs-parent") + NumberOfAdditionalOrganizations);
+                }
+                e.id += NumberOfAdditionalOrganizations;
+            }else{
+                e.id.slice(0,-1)
+                let shouldSkip = false;
+                elementsThatHaveFor.forEach((f) => {
+                    if (shouldSkip) {
+                        return;
+                    }
+                    if (f.for == e.id){
+                        f.for = e.id + NumberOfAdditionalOrganizations;
+                        shouldSkip = true;
+                        return;
+                    }
+                });
+                if(e.hasAttribute('data-bs-target')){
+                    e.setAttribute('data-bs-target', e.getAttribute("data-bs-target") + NumberOfAdditionalOrganizations);
+                }
+                if(e.hasAttribute('aria-controls')){
+                    e.setAttribute('aria-controls', e.getAttribute("aria-controls") + NumberOfAdditionalOrganizations);
+                }
+                if(e.hasAttribute('data-bs-parent')){
+                    e.setAttribute('data-bs-parent', e.getAttribute("data-bs-parent") + NumberOfAdditionalOrganizations);
+                }
+                e.id += NumberOfAdditionalOrganizations;
+            }
+        }
+    });
+    NumberOfAdditionalOrganizations += 1;
+
+    let tempcloseButton = `<button type="button" class="btn-close" style="position: relative; left: 96%; top: 10px;" aria-label="Close" onclick="this.parentElement.remove()"></button>`
+    var temp = document.createElement('div');
+    temp.innerHTML = tempcloseButton;
+    var closeButton = temp.firstChild;
+
+    organizationCard.insertBefore(closeButton, organizationCard.firstChild);
+    // Insert The New Card Before The Configuration Buttons
+    ConfigGenerator.insertBefore(organizationCard, configurationButtonsNode);
 }
